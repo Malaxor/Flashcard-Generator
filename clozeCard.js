95e6a4ff46e13e781437d4764945acec0e0f7cd8
Malaxor <nogobaduk@gmail.com>
2018-11-29 19:46:02 -0700

    more es6 refactoring

--- clozeCard.js
+++ clozeCard.js
@@ -1,63 +1,59 @@
 
-var inquirer = require("inquirer");
-var fs = require("fs");
+const inquirer = require("inquirer");
+const fs = require("fs");
 
-// cloze cards connstructor
-function clozeCard(full, cloze, partial) {
+class ClozeCard {
 
-	this.full = full;
-	this.cloze = cloze;
-	this.partial = full.replace(cloze, "...");
+	constructor(full, cloze, partial) {
+
+		this.full = full;
+		this.cloze = cloze;
+		this.partial = full.replace(cloze, "...");
+	}	
 };
 
-var clozeArray = [];
-
-// this function will push cloze flash cards into the clozeArray
 function generateCloze() {
+
+	const clozeArray = [];
 	// if we don't push the initial response into an array, the original data will be overriden later
-	fs.readFile("cloze.txt", "utf8", function(error, response) {
-		// if we don't get an error
+	fs.readFile("cloze.txt", "utf8", (error, response) => {
+
 		if(!error) {
-			// store the parsed response (readability) into a variable
-			var data = JSON.parse(response);
-			// run a for-loop through all the data
-			for(var i = 0; i < data.length; i++) {
-				// push the data into the cloze array
-				clozeArray.push(data[i]);
-			}
+			
+			response = JSON.parse(response);
+			response.forEach(item => clozeArray.push(item));
+
 			inquirer.prompt([
 				{
 					name: "full",
-					message: "Type in a trivia fact, then press enter -->"
+					message: "Type in a trivia fact, then press enter."
 				},{
 					name: "cloze",
 					message: "Which part do you want removed?"
 				}
-			]).then(function(answers) {
-				// create a new cloze card
-				var newClozeCard = new clozeCard(answers.full, answers.cloze);
-				// push the new cloze card into the cloze array
+			]).then(card => {
+	
+				const newClozeCard = new ClozeCard(card.full, card.cloze);
 				clozeArray.push(newClozeCard);
-				// ask if the user would like to create another cloze card
+
 				inquirer.prompt([
 					{
 						name: "confirm",
 						type: "confirm",
 						message: "Would you like to generate another cloze card?"
 					}
-				]).then(function(generate) {
-					//if the user chooses to create another card...
-					if(generate.confirm === true) {
+				]).then(generate => {
+					
+					if(generate.confirm) {
 						generateCloze();
 					}
 					else {
-						console.log("++++++++++++++++++++++++\n" + "Some other time, perhaps.");
-						// if we've finished creating cloze cards, copy the cloze array values onto the cloze.txt file
-						fs.writeFile("cloze.txt", JSON.stringify(clozeArray, null, 2), function(error) {
-						// if no error...
-		                    if(!error) {
+						console.log(`+++++++++++++++++++++++++\n Perhaps some other time.`);
 
-							  console.log("++++++++++++++++++++++++\n" + "Added new content!" + "\n++++++++++++++++++++++++");
+						fs.writeFile("cloze.txt", JSON.stringify(clozeArray, null, 2), error => {
+
+		                    if(!error) {
+								console.log(`+++++++++++++++++++++++++\n Added new content! \n++++++++++++++++++++++++`);
 		                    }
 		                });
 					}
@@ -69,30 +65,27 @@ function generateCloze() {
 // call this function to read the cloze cards
 function readCloze() {
 	
-	fs.readFile("cloze.txt", "utf8", function(error, response) {
-		// if we don't get an error
+	fs.readFile("cloze.txt", "utf8", (error, response) => {
+	
 		if(!error) {
-			// store the parsed response (json object = better readability) into a new varbiable
-			var data = JSON.parse(response);
-			// store the response's length into a new variable
-			var length = data.length;
-			// randomly choose an index from the object(response)
-			var random = Math.floor(Math.random() * length);
-			// utilize inquirer prompt to retrieve cloze cards
+			
+			response = JSON.parse(response);
+			const random = Math.floor(Math.random() * response.length);
+		
 			inquirer.prompt([
 				{
 					name: "text",
-					message: data[random].partial 
+					message: response[random].partial 
 				},{
 					name: "confirm",
 					type: "confirm",
-					message: "Type 'yes/y' to check your answer"
+					message: "Type 'y' to check your answer."
 				}
-			]).then(function(answer) {
-				// if the user confirms 
-				if(answer.confirm === true) {
-					//display the full text 
-					console.log(data[random].full);
+			]).then(answer => {
+			
+				if(answer.confirm) {
+					
+					console.log(response[random].full);
 					// ask the user if he/she would like another cloze card
 					inquirer.prompt([
 						{
@@ -100,10 +93,10 @@ function readCloze() {
 							type: "confirm",
 							message: "Would you like another cloze card?"
 						}
-					]).then(function(more) {
-						// if the user confirm's..
-						if(more.confirm === true) {
-							// run the readCloze function again
+					]).then(card => {
+					
+						if(card.confirm) {
+						
 							readCloze();
 						}
 						else {
@@ -113,7 +106,7 @@ function readCloze() {
 				}
 				else {
 
-					console.log("\nOne must occasionally pause in order to continue")
+					console.log("\nOne must occasionally pause in order to continue.")
 				}		
 			});
 		}
@@ -122,7 +115,7 @@ function readCloze() {
 
 module.exports = {
 
-	clozeCard,
+	ClozeCard,
 	generateCloze,
 	readCloze
 };
